897177e4b69346d6c6602ba3e299f0fcb51a8343
huype1 <blockerhuy7704@gmail.com>
2024-01-30 17:50:54 +0700

    Still course info but up to step 5

--- part1/courseinfo1.2/src/App.jsx
+++ part1/courseinfo1.2/src/App.jsx
@@ -1,58 +1,65 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
-//this is full of bugs bullshit
-
+const Header = (props) => {
+  return (
+      <h1>{props.course.name}</h1>
+  )
+}
 const Part = (props) => {
   return (
     <div>
     <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+    {props.part1} {props.exercises1}
+    </p>
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+    <p>
+      {props.part3} {props.exercises3}
+    </p> 
     </div>
   )
 }
 const Content = (props) => {
+  console.log(props.course.parts[2].exercises)
   return (
     <div>
-      <Part part1={props.part1} exercises1={props.exercises1}/>
-      <Part part2={props.part2} exercises2={props.exercises2}/>
-      <Part part3={props.part3} exercises3={props.exercises3}/>
-    </div>
-  )
-}
-const Header = (props) => {
-  
-  return (
-      <h1>{props.course}</h1>
-  )
+    <Part part1={props.course.parts[0].name} exercises1={props.course.parts[0].exercises}/>
+    <Part part2={props.course.parts[1].name} exercises2={props.course.parts[1].exercises}/>
+    <Part part3={props.course.parts[2].name} exercises3={props.course.parts[2].exercises}/>
+  </div>)
 }
 const Total = (props) => {
   return (
-    <><p>Number of exercises {props.exercises}</p></>
+    <><p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p></>
   ) 
 }
 //you can only make a react component with a capitalize variable
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamenteals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercises={exercises1 + exercises2 + exercises3}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
