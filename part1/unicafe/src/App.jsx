af6f67a88201eb15f0c036f1cba2d88bf9e5c1e3
huype1 <blockerhuy7704@gmail.com>
2024-01-31 23:48:15 +0700

    better exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,23 +1,31 @@
 import { useState } from 'react'
-
+const Button = (props) => {
+  return(
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+const Display = (props) => {
+  return (
+    <div>{props.good} {props.neutral} {props.bad}</div>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const addGood = () => setGood(good + 1)
+  const addNeutral = () => setNeutral(neutral + 1)
+  const addBad = () => setBad(bad + 1)
   return (
     <div>
-      <div>{good} {neutral} {bad}</div>
-      <button onClick={() => setGood(good + 1)}>
-        Good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        Neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        Bad
-      </button>
+      <Display good={good} neutral={neutral} bad={bad}/>
+      <Button onClick={addGood} text="Good"/>
+      <Button onClick={addNeutral} text="neutral"/>
+      <Button onClick={addBad} text="Bad"/>
     </div>
   )
 }
