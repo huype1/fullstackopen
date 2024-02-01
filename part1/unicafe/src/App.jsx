ce88ba640328c83edc46d73bddd2391f6a546d61
huype1 <blockerhuy7704@gmail.com>
2024-02-01 23:50:24 +0700

    Unicafe step 3 or exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,11 +6,24 @@ const Button = (props) => {
     </button>
   )
 }
-const Display = (props) => {
+const Statistic = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+  const total = good + neutral + bad
+  const average = (good + neutral*0 - 1*bad)/total
+  const positive = good /(total)*100
   return (
-    <div>{props.good} {props.neutral} {props.bad}</div>
+    <div>
+      <h1>statistic</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
   )
-}
+} 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -22,10 +35,11 @@ const App = () => {
   const addBad = () => setBad(bad + 1)
   return (
     <div>
-      <Display good={good} neutral={neutral} bad={bad}/>
+      <h1>give feedback</h1>
       <Button onClick={addGood} text="Good"/>
       <Button onClick={addNeutral} text="neutral"/>
       <Button onClick={addBad} text="Bad"/>
+      <Statistic good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
