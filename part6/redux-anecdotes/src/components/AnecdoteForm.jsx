2c206d2f15a0f24d2190c4cb86361a6f73d3a73e
huype1 <blockerhuy7704@gmail.com>
2024-07-11 01:36:12 +0700

    Fished part 6 last exercise make a notification with react context state management

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -3,45 +3,40 @@ import { setNotification } from '../reducers/notificationReducer'
 import { useDispatch, useSelector } from 'react-redux'
 const Anecdote = ({ anecdote }) => {
   const dispatch = useDispatch()
-  
+
   const vote = () => {
     dispatch(voteForAnecdote(anecdote))
     dispatch(setNotification(`you voted '${anecdote.content}'`, 5))
   }
 
-    return (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
+  return (
+    <div key={anecdote.id}>
+      <div>{anecdote.content}</div>
 
-          <div>
-            has {anecdote.votes}
-            <button onClick={vote}>vote</button>
-          </div>
-          
-        </div>
-      )
+      <div>
+        has {anecdote.votes}
+        <button onClick={vote}>vote</button>
+      </div>
+    </div>
+  )
 }
 const AnecdoteForm = () => {
-    
-    
-    const anecdotes = useSelector(({ filter, anecdotes }) => {
-  
-      if (filter === '' ) {
-        return anecdotes
-      }
-      return anecdotes.filter(particle => particle.content.includes(filter))
-    })
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if (filter === '') {
+      return anecdotes
+    }
+    return anecdotes.filter((particle) => particle.content.includes(filter))
+  })
 
-    
-
-    return (
-            <div>
-            {   [...anecdotes].sort().sort((a, b) => a.votes - b.votes )
-                .map((anecdote) => 
-                    <Anecdote key={anecdote.id} anecdote={anecdote} />
-                )
-            }
-            </div>
-        )
+  return (
+    <div>
+      {[...anecdotes]
+        .sort()
+        .sort((a, b) => a.votes - b.votes)
+        .map((anecdote) => (
+          <Anecdote key={anecdote.id} anecdote={anecdote} />
+        ))}
+    </div>
+  )
 }
 export default AnecdoteForm
